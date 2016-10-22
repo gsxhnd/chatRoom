37c33e34c4efbf29f2f07501f2b6a5836fb9b95f
gsxhnd <gsxhnd@gmail.com>
2016-10-23 00:54:21 +0800

    update

--- client/messageBox.py
+++
@@ -1,6 +0,0 @@
-from PyQt5 import QtWidgets
-class showMessageBox(QtWidgets):
-    def __init__(self):
-        super().__init__()
-    def error(self):
-        error_message = QMessageBox.information(self,'rr')
